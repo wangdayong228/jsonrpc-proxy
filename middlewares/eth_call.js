6decfbdeb0c6885c0cf6d73b62f67f6be56f281c
Pana <pana.wang@outlook.com>
2024-08-06 14:55:16 +0800

    fix estimate rpc compatibility issue

--- middlewares/eth_call.js
+++ middlewares/eth_call.js
@@ -1,7 +1,7 @@
 
 module.exports = async function(ctx, next) {
 
-    if (ctx.request.rpcMethod === 'eth_call') {
+    if (ctx.request.rpcMethod === 'eth_call' || ctx.request.rpcMethod === 'eth_estimateGas') {
         if (ctx.request.body.params[0] && ctx.request.body.params[0].input) {
             ctx.request.body.params[0].data = ctx.request.body.params[0].input;
         }
