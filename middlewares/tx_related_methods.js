ebee98b61550ce0f6831698f51d36e06d481d889
Pana <pana.wang@outlook.com>
2024-08-08 18:23:05 +0800

    adapt tx

--- middlewares/tx_related_methods.js
+++ middlewares/tx_related_methods.js
@@ -8,17 +8,31 @@ module.exports = async function (ctx, next) {
 
     if (method === 'eth_getTransactionByHash' || method === 'eth_getTransactionByBlockHashAndIndex' || method === 'eth_getTransactionByBlockNumberAndIndex') {
         if (ctx.body.result && ctx.body.result.yParity) {
-            ctx.body.result.v = ctx.body.result.yParity;
+            ctx.body.result = adaptTx(ctx.body.result);
         }
     }
 
     if (method === 'eth_getBlockByHash' || method === 'eth_getBlockByNumber') {
         if (ctx.body.result && ctx.body.result.transactions.length > 0 && typeof ctx.body.result.transactions[0] === 'object') {
             for(let i = 0; i < ctx.body.result.transactions.length; i++) {
-                if (ctx.body.result.transactions[i].yParity) {
-                    ctx.body.result.transactions[i].v = ctx.body.result.transactions[i].yParity;
-                }
+                ctx.body.result.transactions[i] = adaptTx(ctx.body.result.transactions[i]);
             }
         }
     }
+}
+
+function adaptTx(tx) {
+    if (tx.yParity) {
+        tx.v = tx.yParity;
+    }
+
+    if (tx.r === '0x0') {
+        tx.r = '0xffffffff';
+    }
+
+    if (tx.s === '0x0') {
+        tx.s = '0xffffffff';
+    }
+
+    return tx;
 }
